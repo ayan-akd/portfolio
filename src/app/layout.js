a90e91f0beae29c7e0003554263987434d6361c7
akd420 <ayanakd112@gmail.com>
2023-12-15 03:45:09 +0600

    added speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <SpeedInsights/>
       </body>
     </html>
   );
