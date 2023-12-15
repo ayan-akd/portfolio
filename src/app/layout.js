4dd1abe99f9bec59b0de23502eafe69912d47e80
akd420 <ayanakd112@gmail.com>
2023-12-16 01:10:13 +0600

    added speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,6 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: 'Portfolio | Ayan Kumar Das',
-//   description: 'Portfolio of Ayan Kumar Das',
-// }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
